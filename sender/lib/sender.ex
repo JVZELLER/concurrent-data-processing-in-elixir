6bf12ba735728d091112389fdbd209f63a542a02
José Victor Zeller <josevictorzg@gmail.com>
2023-11-28 22:53:58 -0300

    chore: chapter 1 - Starting Tasks and Retrieving Results

--- sender/lib/sender.ex
+++ sender/lib/sender.ex
@@ -3,16 +3,21 @@ defmodule Sender do
   Documentation for `Sender`.
   """
 
-  @doc """
-  Hello world.
+  @spec send_email(String.t()) :: {:ok, String.t()}
+  def send_email(email) do
+    3 |> :timer.seconds() |> Process.sleep()
 
-  ## Examples
+    IO.puts("Email to #{email} sent")
 
-      iex> Sender.hello()
-      :world
+    {:ok, "email_sent"}
+  end
 
-  """
-  def hello do
-    :world
+  @spec notify_all([String.t()]) :: :ok
+  def notify_all(emails) do
+    emails
+    |> Enum.map(fn email ->
+      Task.async(fn -> send_email(email) end)
+    end)
+    |> Enum.map(&Task.await/1)
   end
 end
